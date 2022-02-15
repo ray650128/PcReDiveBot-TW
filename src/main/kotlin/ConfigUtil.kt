2f66f3542aad58f7ec4d93bb01802d95ba161e02
Raymond Yang <ray650128@gmail.com>
2022-02-15 12:27:14 +0800

    加入ConfigUtil，並初步啟動jda

---
+++ src/main/kotlin/ConfigUtil.kt
@@ -0,0 +1,29 @@
+import com.google.gson.Gson
+import model.Configure
+import java.io.File
+
+class ConfigUtil {
+    private val configFile = File("config.json")
+
+    lateinit var config: Configure
+        private set
+
+    fun init(): Boolean {
+        if (!configFile.exists()) {
+            println("找不到 config.json 檔案，\n" +
+                    "請依照 config.example.json 檔案建立 config.json 後，再啟動本程式。")
+            return false
+        }
+
+        val configRawText = configFile.readText(Charsets.UTF_8)
+        if (configRawText.isEmpty()) {
+            println("config.json 檔案空白，\n" +
+                    "請依照 config.example.json 檔案建立 config.json 後，再啟動本程式。")
+            return false
+        }
+
+        config = Gson().fromJson(configRawText, Configure::class.java)
+
+        return true
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,27 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.dv8tion.jda.api.requests.GatewayIntent
+import net.dv8tion.jda.api.utils.ChunkingFilter
+import net.dv8tion.jda.api.utils.cache.CacheFlag
+import kotlin.system.exitProcess
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+private val botEventListener = object : ListenerAdapter() {}
+
+fun main(args: Array<String>) {
+    val configUtil = ConfigUtil()
+    if (!configUtil.init()) {
+        exitProcess(0)
+    }
+
+    val jda = JDABuilder.createDefault(configUtil.config.token)
+    jda.apply {
+        disableCache(CacheFlag.ACTIVITY, CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE)
+        setChunkingFilter(ChunkingFilter.NONE)
+        enableIntents(
+            arrayListOf(
+                GatewayIntent.GUILD_MESSAGES
+            )
+        )
+        addEventListeners(botEventListener)
+    }.build()
 }
